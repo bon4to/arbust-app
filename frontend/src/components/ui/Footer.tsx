9aa8bbfc11cb646fd40b78ed81912b339551a3df
Lucas G. Bonato <lucasbon4to@gmail.com>
2025-07-20 14:12:23 -0300

    refactor: update layout to use Header and Footer components
    Extracted header and footer into separate reusable components (Header.tsx and Footer.tsx) and updated BaseLayout to use them.

---
+++ frontend/src/components/ui/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-neutral-900 border-t border-neutral-800 mt-16">
+        <div className="container mx-auto py-8 px-4 md:px-8 text-center text-neutral-500">
+            <p>&copy; {new Date().getFullYear()} Arbust.
+            <br />Made by <a href="https://github.com/lucas-bonato" target="_blank" className="text-primary hover:text-primary-dark transition-colors duration-300">@lucas-bonato</a>.</p>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
