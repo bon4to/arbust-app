dd7f2905bb1fc13029de638078f63aa3756939cd
Lucas G. Bonato <lucasbon4to@gmail.com>
2025-07-25 11:33:49 -0300

    feat: add Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import BaseLayout from "../layouts/BaseLayout";
 import ContentLayout from "../layouts/ContentLayout";
-import HeroSection from "../components/home/HeroSection";
 import TrendingSection from "../components/home/TrendingSection";
 
-export default function Welcome() {
+export default function Home() {
   return (
     <BaseLayout>
-      <HeroSection />
       <ContentLayout >
         <TrendingSection />
       </ContentLayout>
