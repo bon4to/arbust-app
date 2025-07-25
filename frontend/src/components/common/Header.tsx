e064f223d89336662604001224a7fb61d8d5c9df
Lucas G. Bonato <129971622+bon4to@users.noreply.github.com>
2025-07-25 16:28:56 -0300

    feat: add authenticated buttons to Header (#28)

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import arbustLogoUrl from "../../assets/arbust-typo-hollow.svg";
 import { Link } from "react-router-dom";
+import { isAuthenticated } from "../../mock/auth";
+import { Search } from "lucide-react";
 
 const Header: React.FC = () => {
   return (
@@ -10,12 +12,43 @@ const Header: React.FC = () => {
           <img src={arbustLogoUrl} alt="arbust. logo" className="h-6 w-32" />
         </Link>
         <div className="flex items-center space-x-4">
-          <Link to={"/login"} className="text-neutral-300 hover:text-white transition-colors">
-            Login
-          </Link>
-          <Link to={"/login"} className="bg-primary hover:bg-primary-dark text-white font-bold py-2 px-5 rounded-full transition-colors">
-            Sign Up
-          </Link>
+          { isAuthenticated() ? (
+            <>
+              <button
+                className="text-neutral-400 tracking-widest uppercase text-sm font-bold hover:text-primary transition"
+              >
+                Activity
+              </button>
+              <button
+                className="text-neutral-400 tracking-widest uppercase text-sm font-bold hover:text-primary transition"
+              >
+                Songs
+              </button>
+              <button
+                className="text-neutral-400 tracking-widest uppercase text-sm font-bold hover:text-primary transition"
+              >
+                <Search className="inline-block stroke-current-600 h-5 mr-1" />
+              </button>
+              <span className="text-white tracking-widest uppercase text-sm font-bold">
+                Welcome, {'User'}!
+              </span>
+            </>
+          ) : (
+            <>
+              <Link
+                to="/login"
+                className="text-neutral-300 hover:text-white transition-colors"
+              >
+                Login
+              </Link>
+              <Link
+                to="/login"
+                className="bg-primary hover:bg-primary-dark text-white font-bold py-2 px-5 rounded-full transition-colors"
+              >
+                Sign Up
+              </Link>
+            </>
+          )}
         </div>
       </nav>
     </header>
