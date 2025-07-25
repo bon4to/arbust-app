3b2c1caeea2f4b3d3504bba9fbd69a6ba9237211
Lucas G. Bonato <lucasbon4to@gmail.com>
2025-07-25 11:34:49 -0300

    add Home page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import AuthPage from "./pages/AuthPage";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/welcome" element={<Welcome />} />
         <Route path="/login" element={<AuthPage />} /> 
+        <Route path="/home" element={<Home />} /> 
       </Routes>
     </BrowserRouter>
   );
