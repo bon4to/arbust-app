caf283ca019e64994a8f28c385681b4b351ecdb9
Lucas G. Bonato <129971622+bon4to@users.noreply.github.com>
2025-08-21 20:00:58 -0300

    Feat/download page (#31)
    * feat: add download page
    
    * chore: add react-icons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Welcome from "./pages/Welcome";
 import AuthPage from "./pages/AuthPage";
 import Home from "./pages/Home";
 import Album from "./pages/AlbumPage";
+import DownloadPage from "./pages/DownloadPage";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<AuthPage />} /> 
         <Route path="/home" element={<Home />} /> 
+        <Route path="/download" element={<DownloadPage />} /> 
         <Route path="/album/:id" element={<Album />} /> 
       </Routes>
     </BrowserRouter>
